9a09e2ace9826db0d61b7305c65482231f9df011
stefvdwater <stef.vandewater@gmail.com>
2026-05-28 14:03:50 +0200

    Maak waarschuwingsicoon symmetrisch in viewBox en ruim duplicate op

--- src/renderer/src/components/icons/IconWarn.tsx
+++ src/renderer/src/components/icons/IconWarn.tsx
@@ -22,9 +22,10 @@ export function IconWarn({ size = 14 }: Props): JSX.Element {
       strokeLinecap="round"
       strokeLinejoin="round"
       aria-hidden="true"
+      style={{ display: 'block', flexShrink: 0 }}
     >
-      <path d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0Z" />
-      <path d="M12 9v4M12 17h.01" />
+      <path d="M10.29 2 1.82 19a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 2a2 2 0 0 0-3.42 0Z" />
+      <path d="M12 8v5M12 18h.01" />
     </svg>
   )
 }
